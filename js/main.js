e69eaccfa57450dd660e76905babfa975c610079
CJayXYZ <chandrajeet29@gmail.com>
2020-02-19 22:37:10 +0530

    JS implimentation

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function wait(ms)
+{
+var d = new Date();
+var d2 = null;
+do { d2 = new Date(); }
+while(d2-d < ms);
+}
+
+
+let i = 0
+
+function loop() {
+    // wait(1000/i)
+    let tag = document.querySelectorAll('div')[5]
+    tag.innerHTML = i
+    size = (.1*i) + 'em'
+    tag.style.fontSize = size
+    i++
+    // console.log(i);
+    if (i <800 +1) {
+    window.requestAnimationFrame(loop) 
+}
+}
+
+loop()
